1682fde0421306d58c71103b3a210dbd4be21a9d
Saltanat Degenbaeva <sdegenbaeva@usf.edu>
2019-09-05 21:30:32 -0400

    Version 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,9 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadKey();
-        }
-    }
-}
+            // Pause to display Hello World!
+            Console.WriteLine("Hello World! My name is Salta!");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }// end of Main
+    }// end of class
+}// end of namespace
